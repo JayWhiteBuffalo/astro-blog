c2b454c45d2cc2e04c752861b5eccee80cceed8a
White Buffalo <apothocare@gmail.com>
2023-01-26 14:22:06 -0600

    Got carosel to work by importing files in index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind({
+        config: {path: './tailwind.config.cjs'}
+    })],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,10 @@
 module.exports = {
-    mode: 'jit',
-    purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
-  };
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+  content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  plugins: [
+    require('tw-elements/dist/plugin'),
+
+  ]
+}
+
