95228c72b20a153a62f2db9b3e8d198db241c62a
White Buffalo <apothocare@gmail.com>
2023-01-26 16:18:21 -0600

    Added code needed to deploy to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+    site: "http://JayWhiteBuffalo.github.io",
+    base: '/astro-blog',
     integrations: [tailwind({
         config: {path: './tailwind.config.cjs'}
     })],
